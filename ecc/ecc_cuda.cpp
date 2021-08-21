1e06e24866d2e80c2e54c8d975aadce561a091ab
levyben <levyben1@gmail.com>
2021-08-21 17:09:52 +0300

    some cleanups

--- ecc/ecc_cuda.cpp
+++ ecc/ecc_cuda.cpp
@@ -355,54 +355,6 @@ static void update_warping_matrix_ECC (Mat& map_matrix, const Mat& update, const
 }
 
 
-/** Function that computes enhanced corelation coefficient from Georgios et.al. 2008
-*   See https://github.com/opencv/opencv/issues/12432
-*/
-double cv::computeECC(InputArray templateImage, InputArray inputImage, InputArray inputMask)
-{
-    CV_Assert(!templateImage.empty());
-    CV_Assert(!inputImage.empty());
-
-    if( ! (templateImage.type()==inputImage.type()))
-        CV_Error( Error::StsUnmatchedFormats, "Both input images must have the same data type" );
-
-    Scalar meanTemplate, sdTemplate;
-
-    int active_pixels = inputMask.empty() ? templateImage.size().area() : countNonZero(inputMask);
-    int type = templateImage.type();
-    meanStdDev(templateImage, meanTemplate, sdTemplate, inputMask);
-    Mat templateImage_zeromean = Mat::zeros(templateImage.size(), templateImage.type());
-    Mat templateMat = templateImage.getMat();
-    Mat inputMat = inputImage.getMat();
-
-    /*
-     * For unsigned ints, when the mean is computed and subtracted, any values less than the mean
-     * will be set to 0 (since there are no negatives values). This impacts the norm and dot product, which
-     * ultimately results in an incorrect ECC. To circumvent this problem, if unsigned ints are provided,
-     * we convert them to a signed ints with larger resolution for the subtraction step.
-     */
-    if(type == CV_8U || type == CV_16U) {
-        int newType = type == CV_8U ? CV_16S : CV_32S;
-        Mat templateMatConverted, inputMatConverted;
-        templateMat.convertTo(templateMatConverted, newType);
-        cv::swap(templateMat, templateMatConverted);
-        inputMat.convertTo(inputMatConverted, newType);
-        cv::swap(inputMat, inputMatConverted);
-    }
-    subtract(templateMat, meanTemplate, templateImage_zeromean, inputMask);
-    double templateImagenorm = std::sqrt(active_pixels*sdTemplate.val[0]*sdTemplate.val[0]);
-
-    Scalar meanInput, sdInput;
-
-    Mat inputImage_zeromean = Mat::zeros(inputImage.size(), inputImage.type());
-    meanStdDev(inputImage, meanInput, sdInput, inputMask);
-    subtract(inputMat, meanInput, inputImage_zeromean, inputMask);
-    double inputImagenorm = std::sqrt(active_pixels*sdInput.val[0]*sdInput.val[0]);
-
-    return templateImage_zeromean.dot(inputImage_zeromean)/(templateImagenorm*inputImagenorm);
-}
-
-
 double findTransformECCGpu_(InputArray templateImage,
                             InputArray inputImage,
                             InputOutputArray warpMatrix,
